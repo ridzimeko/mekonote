43db33bb29ff38f5f2f5d11e893f645fc91f9102
Ridzimeko <ridzimeko@slayerwitch.my.id>
2023-07-27 08:45:39 +0700

    remove imagemin dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,3 @@
-import viteImageMin from "vite-plugin-imagemin";
-
 export default {
-  plugins: [
-    viteImageMin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 40,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-    }),
-  ],
-};
+  plugins: [],
+}
